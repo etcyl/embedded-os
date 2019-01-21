d2101308ca10f7d50da36145a3a8258b07f721e5
Matt Fleetwood <32446381+etcyl@users.noreply.github.com>
2019-01-20 18:18:08 -0800

    Update end.c

--- assignment2/end.c
+++ assignment2/end.c
@@ -3,6 +3,7 @@ Matt Fleetwood
 1/19/2019
 Portland, OR
 Assignment 2 for ECE 362
+
 Accepts a filename and an optional n integer as input to the program.
 If the filename does not exist, an error message is printed to stdout.
 If the filename exists and n is larger than 0 but less than 10, the 
@@ -10,7 +11,6 @@ last n lines from the file are displayed to stdout.
 If n is less than 1 or greater than 9, an error is shown to stdout.
 If file exists but n is not specified, then the last 5 lines of the
 file are displayed to stdout.
-
 **/
 
 #include <stdio.h>
@@ -24,7 +24,7 @@ int main(int argc, char* argv[])
 {
         int fd = open(argv[1], O_RDONLY); // Get the filename
         int n;  // int n = atoi(argv[2]); // Get the value for n
-    int current_char = 0, char_count = 0, done = 0;
+        int current_char = 0, char_count = 0, done = 0;
         char to_display[RD_LEN], buff[FILE_CHAR_LIMIT];        
         
         if(argc != 3)
@@ -34,34 +34,28 @@ int main(int argc, char* argv[])
         
 
         if(fd < 0){ // File does not exist
-        write(2, "ERROR: filename does not exist.\n", 31);
+                write(2, "ERROR: filename does not exist.\n", 31);
                 return -1;
-    }         
-    else if(n > 0 && n < 10){ // File exists, n is a valid number
-                //write(1, "Reading from existing file...\n", 31);
-                
-                //Start by getting the total character count in the file
+        }         
+        else if(n > 0 && n < 10){ // File exists, n is a valid number
                 char_count = read(fd, buff, FILE_CHAR_LIMIT);
                 if( char_count >1 )
                    char_count -= 1;
-               // printf("char_count is: %d\n", char_count);
                 close(fd);           
-        fd = open(argv[1], O_RDONLY);
+                fd = open(argv[1], O_RDONLY);
                 if(char_count <= n)
                   {
-            while(!done)
-                     { // Show the characters to the user on stdout
-               // current_char = write(1, to_display, current_char);
+                   while(!done)
+                     { 
                         if( (current_char = read(fd, to_display, RD_LEN)) !=0)
                           done = 0;
                         else
                           done = 1;
-
                         current_char = write(1, to_display, current_char);
                      }
-             close(fd);
+                close(fd);
                    }
-                 else if(char_count > n)
+                else if(char_count > n)
                    { 
                      lseek(fd, char_count - n, SEEK_CUR);
                      while(!done)
@@ -74,21 +68,23 @@ int main(int argc, char* argv[])
                         current_char = write(1, to_display, current_char);
                       }
                    }
-                  else
+                 else
                    { //Default case
+                     write(2, "Default case reached. Terminating program...\n", 55);
                      close(fd);
+                     return -1;
                    }
-            return 0;
-    }
+                return 0;
+        }
         else if(n <= 0 || n >= 10){ // n is an invalid number
                 write(2, "ERROR: n can only be 1 - 9.\n", 28);
                 close(fd);
-        return -1;
-    }
+                return -1;
+        }
         else{ // Default case
-        write(2, "Default case reached. Terminating program...\n", 55);
-        return -1;
-    }
+                write(2, "Default case reached. Terminating program...\n", 55);
+                return -1;
+        }
 
     return 0;
 }
